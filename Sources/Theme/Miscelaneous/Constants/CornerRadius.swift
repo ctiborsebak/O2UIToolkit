245b1b2103e823c5d931b7bfa50acd5928b11ca6
ctiborsebak <sebakctibor@gmail.com>
2025-04-09 13:19:54 +0200

    Add dimensions (#3)

---
+++ Sources/Theme/Miscelaneous/Constants/CornerRadius.swift
@@ -0,0 +1,8 @@
+import CoreGraphics
+
+public extension CGFloat {
+  struct CornerRadius {
+    /// 12 px
+    public static let input: CGFloat = 12
+  }
+}
---
+++ Sources/Theme/Miscelaneous/Constants/Spacing.swift
@@ -0,0 +1,14 @@
+import CoreGraphics
+
+public extension CGFloat {
+  struct Spacing {
+    /// 8 px
+    public static let XS: CGFloat = 8
+    /// 12 px
+    public static let S: CGFloat = 12
+    /// 16 px
+    public static let M: CGFloat = 16
+    /// 20 px
+    public static let L: CGFloat = 20
+  }
+}
